59202544aa321b29689b1f66adf0caa52f30b876
Donald Lee <donaldlee50@gmail.com>
2015-05-11 02:33:08 -0700

    Contact model testing done

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -6,12 +6,16 @@ FactoryGirl.define do
 	  	f.address2 "86B"
 	  	f.city "Vancouver"
 	  	f.postal_code "V5T 6C7"
-	  	f.typ_country_id "124" 
-	  	f.typ_region_id "527" 
-	    association :email_addresses, factory: :email_addresses
-	    association :telephone_numbers, factory: :telephone_numbers
-	    association :typ_region, factory: :typ_region 
-	    association :typ_country, factory: :typ_country
-	    association :typ_region, factory: :typ_region
+	  	
+	  	association :typ_country, factory: :typ_country
+   		association :typ_region, factory: :typ_region
 	end
+
+	factory :typ_country, :class => 'TypCountry' do
+	    country "Canada"
+	end
+
+	factory :typ_region, :class => 'TypRegion' do
+    	name "Alberta"
+  	end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -6,4 +6,50 @@ RSpec.describe Contact, type: :model do
   	}
 
   	subject { @contact }
+
+  	it { should belong_to(:typ_country)}
+  	it { should belong_to(:typ_region)}
+
+  	it { should have_many(:email_addresses)}
+  	it { should have_many(:telephone_numbers)}
+
+  	it { should accept_nested_attributes_for(:email_addresses)}
+  	it { should accept_nested_attributes_for(:telephone_numbers)}
+
+  	describe "when typ_country is not present" do
+		before { @contact.typ_country_id = " " }
+	    it { should_not be_valid }
+	end
+
+	describe "when typ_region is not present" do
+		before { @contact.typ_region_id = " " }
+	    it { should_not be_valid }
+	end
+
+	describe "when first name is not present" do
+		before { @contact.first_name = " " }
+	    it { should_not be_valid }
+	end
+
+	describe "when last name is not present" do
+		before { @contact.last_name = " " }
+	    it { should_not be_valid }
+	end
+
+	describe "when address1 is not present" do
+		before { @contact.address1 = " " }
+	    it { should_not be_valid }
+	end
+
+	describe "when city is not present" do
+		before { @contact.city = " " }
+	    it { should_not be_valid }
+	end
+
+	describe "when postal code is not present" do
+		before { @contact.postal_code = " " }
+	    it { should_not be_valid }
+	end
+
+
 end
